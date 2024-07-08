bbd949903f40b5c75a068ab9a2fbf6869c83ca15
bnbnac <bnbnac@naver.com>
2024-07-08 10:30:16 +0900

    init

---
+++ src/main/java/hanghae/pre/bnbnac/BnbnacApplication.java
@@ -0,0 +1,13 @@
+package hanghae.pre.bnbnac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BnbnacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BnbnacApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hanghae/pre/bnbnac/BnbnacApplicationTests.java
@@ -0,0 +1,13 @@
+package hanghae.pre.bnbnac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BnbnacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
